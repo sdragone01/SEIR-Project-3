fc45f64cf84d35af81078ce01f2c892035011694
christopher chapa <chapachristopher16@gmail.com>
2022-07-01 12:41:16 -0500

    add actual infrastructure

---
+++ project3-mern-infrastructure/src/pages/App/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistory/OrderHistoryPage';
+import NavBar from '../../components/NavBar';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
+
+
+
+export default function App() {
+  const [user, setUser] = useState(getUser())
+  console.log(user)
+  return (
+    <main className="App">
+      {user ? 
+        <>
+          <NavBar user={user} setUser={setUser}/>
+          <Routes>
+            <Route path='/orders/new' element={<NewOrderPage/>} />
+            <Route path='/orders' element={<OrderHistoryPage/>} />
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser}/>
+      }
+    </main>
+  );
+}
+
+
