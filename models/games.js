ffac55458f80257dd30b6066ae1738fb2218fb9f
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-07-05 10:20:20 -0400

    game schema and library schema added

---
+++ models/games.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const gameSchema = new Schema({
+    title:
+    {
+        type: String,
+        required: true,
+    },
+    genre:
+    {
+        type: String,
+        required: true,
+    },
+    description:
+    {
+        type: String,
+        required: true,
+    },
+    link:
+    {
+        type: String,
+        required: false,
+    },
+    img:
+    {
+        type: String,
+        required: false,
+    }
+}, {
+    timestamps: true,
+
+})
+
+module.exports = mongoose.model('Game', gameSchema)
