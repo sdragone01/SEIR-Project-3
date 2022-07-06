47153a4cd7d38b2b8d8780180e8cb431479c7c3d
christopher chapa <chapachristopher16@gmail.com>
2022-07-06 13:26:17 -0500

    basic form layout

---
+++ src/components/AddGameForm.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+export default function AddGameForm(){
+ const [addGame, setAddGame] = useState({})   
+
+    return(
+    <>
+        <form>
+            <input type="text" value={title} name='title'/>
+            <input type="text" value={genre} name='genre'/>
+            <input type="text" value={description} name='description'/>
+            <input type="text" value={link} name='link'/>
+            <input type="text" value={img} name='img'/>
+        </form>
+    </>
+    )   
+}
