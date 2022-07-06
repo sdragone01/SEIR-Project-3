ed2fa785d7d8fd5561dbdbc782b202c8028a9ff6
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-07-06 15:40:57 -0400

    chris's push

--- src/components/AddGameForm.jsx
+++ src/components/AddGameForm.jsx
@@ -1,15 +1,28 @@
 import { useState } from "react"
 export default function AddGameForm() {
-    const [addGame, setAddGame] = useState({})
+    const [form, setForm] = useState({})
+    const [errors, setErrors] = useState({})
+    const setField = (field, value) => {
+        setForm({
+            ...form, [field]: value
+        })
+        if (!!errors[field])
+            setErrors({ ...errors, [field]: null })
+    }
 
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(form)
+    }
     return (
         <>
-            <form>
-                <input type="text" value={title} name='title' />
-                <input type="text" value={genre} name='genre' />
-                <input type="text" value={description} name='description' />
-                <input type="text" value={link} name='link' />
-                <input type="text" value={img} name='img' />
+            <form onSubmit={ }>
+                <input type="text" onChange={(e) => setField('title', e.target.value)} value={form.title} name='title' />
+                <input type="text" onChange={ } value={genre} name='genre' />
+                <input type="text" onChange={ } value={description} name='description' />
+                <input type="text" onChange={ } value={link} name='link' />
+                <input type="text" onChange={ } value={img} name='img' />
+                <button type='submit'></button>
             </form>
         </>
     )
