28f7db2c45fb47daf1b2324eb5cd7732688ed839
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-07-07 19:28:32 -0400

    backend games now in game index

--- src/pages/GameCard/GameCard.jsx
+++ src/pages/GameCard/GameCard.jsx
@@ -1,24 +1,39 @@
 import axios from "axios";
 import React from "react";
+import { Link } from "react-router-dom"
 
 const baseURL = 'api/games';
 
-export default function GameCard() {
+export default function GameCard({ game }) {
     const [post, setPost] = React.useState(null);
 
     React.useEffect(() => {
-        axios.get(baseURL).then((response) => {
-            console.log(response)
+        axios.get(baseURL).then((post) => {
+            setPost(post)
+            console.log(post.data.title)
 
         });
     }, []);
 
     if (!post) return null;
-    console.log(post.title)
 
     return (
-        <div>
-            <h1>GAME</h1>
+        <div className="GameGrid">
+            {post.data.map((game) => {
+                return (
+                    <Link to={`/games/${game.title}`}>
+
+                        <div className="gameCard" key={game.title}>
+                            <div className="gameTag">
+                                <h2 id="title">{game.title}</h2>
+
+                            </div>
+
+                        </div>
+
+                    </Link>
+                )
+            })}
         </div>
     )
 }
