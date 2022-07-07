94e096327c24c53084bd9e42a21052504bb02050
christopher chapa <chapachristopher16@gmail.com>
2022-07-07 10:05:15 -0500

    more work to connect form to backend

--- src/components/AddGameForm.jsx
+++ src/components/AddGameForm.jsx
@@ -1,6 +1,6 @@
 import './AddGameForm.css'
 import { Component } from "react"
-
+import { addGame } from '../utilities/games-api'
 export default class AddGameForm extends Component {
     
     state = {
@@ -25,6 +25,7 @@ export default class AddGameForm extends Component {
         try {
             const formData = {...this.state}
             delete formData.error
+            await addGame(formData)
         } catch{
             this.setState({error: 'Adding Game Failed - Try Again'})
         }
@@ -36,13 +37,19 @@ export default class AddGameForm extends Component {
         <h1 className='AddGame'>Add Game</h1>
 
             <form className='GameInfo'>
+                Title
                 <input type="text" onChange={this.handleChange} value={this.state.title} name='title' />
+                Genre
                 <input type="text" onChange={this.handleChange} value={this.state.genre} name='genre' />
+                Description
                 <input type="text" onChange={this.handleChange } value={this.state.description} name='description' />
+                Link
                 <input type="text" onChange={this.handleChange} value={this.state.link} name='link' />
+                Image
                 <input type="text" onChange={this.handleChange} value={this.state.img} name='img' />
+                Price
                 <input type="number" onChange={this.handleChange} value={this.state.price} name='price' />
-                <button onClick={this.handleSubmit} type='submit'></button>
+                <button onClick={this.handleSubmit} type='submit'>SUBMIT</button>
             </form>
 
         </>
