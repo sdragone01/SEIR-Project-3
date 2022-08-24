8dcf9cff83187033041dd7eea6cd5244accbc87c
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-08-24 15:21:23 -0400

    2

--- config/database.js
+++ config/database.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URI)
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to database')
+}).catch(err => {
+    console.log(err)
+}
+)
+
 
 const db = mongoose.connection
 
